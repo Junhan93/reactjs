57e2b49a806b13b5668d3114fd3724e721557bb2
Junhan93 <junhan.3008@gmail.com>
2020-11-08 15:02:10 +0800

    React tweet-like changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Tweet from './Tweet';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+// create a component
+function App(){
+
+  const [users, setUsers] = useState([
+    {name: 'Junhan', message:"Hello there", likes:"3.2k"},
+    {name: "Cheryl", message:"Hi!", likes:"3.2k"},
+    {name: "John", message:"I am awesome!", likes:"898"}
+  ]);
+  //based on our state, our HTML is gonna change
+  // const [isRed, setRed] = useState(false);
+  // const [count, setCount] = useState(0);
+
+  // const increment = () => {
+  //   setCount(count + 1);
+  //   setRed(true);
+  // };
+  
+  return(
+    <div className="app">
+        {users.map(user =>(
+          <Tweet name={user.name} message={user.message} likes={user.likes}/>
+        ))}
     </div>
   );
 }
 
+// export the component
 export default App;
+
+// <Tweet name="Junhan" message="This is a first tweet" likes="3.2K"/>
+//       <Tweet name="Cheryl" message="This is a second tweet" likes="6.4K"/>
+//       <Tweet name="Jane" message="This is a third tweet" likes="7.9K"/>
+//       <Tweet name="John" message="This is a fourth tweet" likes="868"/>
+
+
+// <h1 className= { isRed ? "red" : "" }>Change my color!</h1>
+//       <button onClick={ increment }>Increment</button>
+//       <h1>{count}</h1>    {/* project the count here */}
